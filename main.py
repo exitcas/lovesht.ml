634f82aa98a5db0fbc47a5e38373139d16ad74c7
Lucas <83518257+Luqaska@users.noreply.github.com>
2022-08-15 04:16:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ app.config["SERVER_NAME"] = "44ht.ml"
 @app.route("/<path:filename>", subdomain="<user>")
 def roomsd(user, filename):
   c = requests.get(f"https://web.sape.gq/~{user}/{filename}")
-  r = make_response(c.text), c.status_code
+  r = make_response(c.text, c.status_code)
   r.headers["content-type"] = c.headers["content-type"]
   return r
 
 @app.route("/", subdomain="<user>")
 def roomhomesd(user):
   c = requests.get(f"https://web.sape.gq/~{user}/")
-  r = make_response(c.text), c.status_code
+  r = make_response(c.text, c.status_code)
   r.headers["content-type"] = c.headers["content-type"]
   return r
